5e2241a40f471576ed683809044204a468b1c9c2
Nick Semenkovich <semenko@alum.mit.edu>
2014-06-04 17:39:27 -0500

    tweaks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,37 +1,39 @@
 var strains = new Bloodhound({
-  datumTokenizer: function(d) {
+    datumTokenizer: function(d) {
         return Bloodhound.tokenizers.whitespace(d['genus']).concat(Bloodhound.tokenizers.whitespace(d['species']));
-  },
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  limit: 10,
-  prefetch: '../data/93genomes.json'
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    limit: 10,
+    prefetch: '../data/93genomes.json'
 });
 
 // kicks off the loading/processing of `local` and `prefetch`
 strains.initialize();
 
+var typeahead_field = $('.typeahead');
+
 // passing in `null` for the `options` arguments will result in the default
 // options being used
-$('.typeahead').typeahead(null, {
-  name: 'strains',
-  displayKey: function(strain) { return strain['genus'] + " " + strain['species']; },
-  // `ttAdapter` wraps the suggestion engine in an adapter that
-  // is compatible with the typeahead jQuery plugin
-  source: strains.ttAdapter()
+typeahead_field.typeahead(null, {
+    name: 'strains',
+    displayKey: function(strain) { return strain['genus'] + " " + strain['species']; },
+    // `ttAdapter` wraps the suggestion engine in an adapter that
+    // is compatible with the typeahead jQuery plugin
+    source: strains.ttAdapter()
 });
 
 function renderNCBI() {
     console.log('Render triggered.')
 }
 
-$('.typeahead').bind('typeahead:selected', function(obj, datum, name) {
+typeahead_field.bind('typeahead:selected', function(obj, datum, name) {
     console.log(obj);
     console.log(datum);
     console.log(name);
     renderNCBI();
 });
 
-$('.typeahead').bind('keypress', function(event) { if (event.which === 13) { renderNCBI(); } } );
+typeahead_field.bind('keypress', function(event) { if (event.which === 13) { renderNCBI(); } } );
 
 // TODO: REMOVE THIS LINE WHEN IN PROD!
 strains.clearPrefetchCache();
